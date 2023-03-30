bd97f0dd8b9bff656ea7400c44cdc118540f78c3
surathjhakal <jhakal.surath@gmail.com>
2023-03-30 17:49:38 +0530

    fixed env thing

--- pages/login.js
+++ pages/login.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { Button } from "@material-ui/core";
 import { auth, provider } from "../firebase";
 import styles from "../styles/Login.module.css";
@@ -7,15 +8,24 @@ function Login() {
     auth.signInWithPopup(provider).catch(alert);
   };
   return (
-    <div className={styles.login}>
-      <div className={styles.login_content}>
-        <img src="https://images.vexels.com/media/users/3/139911/isolated/preview/1afb4038427b2bd8edd275940aea269d-chat-service-icon.png" />
-        <div className={styles.login_text}>
-          <h1>Sign in to MeChat</h1>
+    <>
+      <Head>
+        <title>MeChat</title>
+        <link
+          rel="icon"
+          href="https://images.vexels.com/media/users/3/139911/isolated/preview/1afb4038427b2bd8edd275940aea269d-chat-service-icon.png"
+        />
+      </Head>
+      <div className={styles.login}>
+        <div className={styles.login_content}>
+          <img src="https://images.vexels.com/media/users/3/139911/isolated/preview/1afb4038427b2bd8edd275940aea269d-chat-service-icon.png" />
+          <div className={styles.login_text}>
+            <h1>Sign in to MeChat</h1>
+          </div>
+          <Button onClick={signIn}>Sign In With Google</Button>
         </div>
-        <Button onClick={signIn}>Sign In With Google</Button>
       </div>
-    </div>
+    </>
   );
 }
 
